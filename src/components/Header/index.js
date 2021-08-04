c03d32b272895cd86da9588e80292b8631e81768
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-08-04 12:25:32 -0300

    add state carrinho e popover

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,57 @@
-import { Box, Flex, HStack, useColorMode } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import { useState } from 'react';
 import { RiShoppingCart2Line, RiMoonClearLine } from 'react-icons/ri';
 
 export function Header() {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { cartItem, isCartItem } = useState(false);
 
   return (
     <Flex as="header" w="100%" pt="8" justify="center" h="10vh">
       <HStack spacing="80">
         <Box as="button" onClick={toggleColorMode}>
           <RiMoonClearLine fontSize="2rem" />
-          {colorMode === 'light' ? 'Dark' : 'Light'}
+          {colorMode === 'light' ? 'Escuro' : 'Claro'}
         </Box>
         <Box as="h1" fontSize="2rem">
           ceubexpress
         </Box>
-        <Box as="button">
-          <RiShoppingCart2Line fontSize="2rem" />
-        </Box>
+
+        <Popover isLazy trigger="hover">
+          <PopoverTrigger>
+            <Button variant="unstyled" fontSize="2rem">
+              <RiShoppingCart2Line as="button" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverHeader fontWeight="semibold">
+              Carrinho de compras
+            </PopoverHeader>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <PopoverBody>
+              {!isCartItem ? (
+                <Text>Nenhum item adicionado</Text>
+              ) : (
+                <Text>Arroz</Text>
+              )}
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
       </HStack>
     </Flex>
   );

2405f83eb8b91488c03ff802a3032fd3744cc750
Vitor <vitor.hw@outlook.com>
2021-08-05 11:49:08 -0300

    close button, wlight and dark theme for the login modal

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,29 +1,33 @@
 import Modal from 'react-modal';
 import { useState } from 'react';
-import { Image, Flex, Spacer } from '@chakra-ui/react';
+import { Image, Flex, Spacer, useColorMode, Icon } from '@chakra-ui/react';
 import { LoginInput } from './LoginInput';
+import { RiCloseFill } from 'react-icons/ri'
 
-const modalStyles = {
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-  },
-  content: {
-    maxWidth: '800px',
-    width: '80%',
-    height: '512px',
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    padding: 'none',
-    margin: 'none',
-    overflow: 'hidden',
-    border: 'none',
-  },
-};
 
 export function LoginModal() {
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const modalStyles = {
+    overlay: {
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    },
+    content: {
+      background: colorMode === 'light' ? 'white' : '#171717',
+      maxWidth: '800px',
+      width: '80%',
+      height: '512px',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      padding: 'none',
+      margin: 'none',
+      overflow: 'hidden',
+      border: 'none',
+    },
+  };
 
   function handleClose() {
     setIsModalOpen(false);
@@ -38,10 +42,18 @@ export function LoginModal() {
     >
       <Flex>
         <Spacer />
-
         <LoginInput />
         <Spacer />
-        <Image height="512px" src="./images/login.jpeg" alt="login-image" />
+        <Image height="512px" src="./images/login_white.jpeg" alt="login-image" />
+        <Icon 
+          cursor="pointer" 
+          fontSize='1.5rem' 
+          position='absolute' 
+          top='0' 
+          right='0'
+          as={RiCloseFill}
+          onClick={handleClose} 
+          />
       </Flex>
     </Modal>
   );
